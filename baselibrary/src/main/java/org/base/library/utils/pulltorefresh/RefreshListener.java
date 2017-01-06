265d69143c1e63b605f8b658e247917604e29c6d
YinShengyi <2174277493@qq.com>
2017-01-06 16:53:36 +0800

    添加下拉刷新功能

---
+++ baselibrary/src/main/java/org/base/library/utils/pulltorefresh/RefreshListener.java
@@ -0,0 +1,14 @@
+package org.base.library.utils.pulltorefresh;
+
+public abstract class RefreshListener implements BaseRefreshListener {
+
+    @Override
+    public void finish() {
+
+    }
+
+    @Override
+    public void finishLoadMore() {
+
+    }
+}
